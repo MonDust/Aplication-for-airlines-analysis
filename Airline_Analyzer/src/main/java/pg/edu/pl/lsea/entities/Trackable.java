c92d6b11d1648548424f037bae9112b00e9cc068
pggit_oidc2222610 <s193584@student.pg.edu.pl>
2025-03-23 13:08:48 +0100

    Fixed inheritance, added comments

--- Airline_Analyzer/src/main/java/pg/edu/pl/lsea/entities/Trackable.java
+++ Airline_Analyzer/src/main/java/pg/edu/pl/lsea/entities/Trackable.java
@@ -1,11 +1,26 @@
 package pg.edu.pl.lsea.entities;
 
+/**
+ * Abstract class representing trackable entities such as flight and aircraft.
+ */
 public abstract class Trackable {
+    /**
+     * Unique icao24 code for identifying aircraft using a 24-bit 6-character hexadecimal format.
+     */
     private String icao24;
 
+    /**
+     * Returns the icao24 code.
+     * @return A string representing the 6-character hexadecimal icao24 code of the trackable entity.
+     */
     public String getIcao24() {
         return icao24;
     }
+
+    /**
+     * Sets the icao24 code.
+     * @param icao24 A string representing the 6-character hexadecimal icao24 code of the trackable entity.
+     */
     public void setIcao24(String icao24) {
         this.icao24 = icao24;
     }
