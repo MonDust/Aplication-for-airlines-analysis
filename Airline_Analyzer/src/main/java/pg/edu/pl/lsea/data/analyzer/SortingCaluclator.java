85dd29e78a5c0622356393c40804edf6d3b5cfc5
StaPink <stapin.4@gmail.com>
2025-03-25 21:07:45 +0100

    Fix: Sorting

--- Airline_Analyzer/src/main/java/pg/edu/pl/lsea/data/analyzer/SortingCaluclator.java
+++ Airline_Analyzer/src/main/java/pg/edu/pl/lsea/data/analyzer/SortingCaluclator.java
@@ -13,36 +13,20 @@ public class SortingCaluclator extends DataAnalyzer  {
     public void analyzeDataForDashbord(List<Aircraft> aircrafts, List<EnrichedFlight> flights) {
         System.out.println("===== Dashboard =====");
         System.out.println("Sorted Aircraft List:");
-        sortAircraft(aircrafts);
 
-        Collections.sort(aircrafts, new Aircraft.AircraftComparator());
-
-        for (Aircraft aircraft : aircrafts) {
-            System.out.println(aircraft);
-        }
-
+        aircrafts.sort(new Aircraft.AircraftComparator());
+        printAircraftList(aircrafts);
 
         System.out.println("\nSorted Flight List:");
-        sortUsingComparator(flights);
-     //  printFlightList(flights);
-    }
-
-    private void sortFlights(List<EnrichedFlight> flights) {
-        Collections.sort(flights);
-    }
-
-    private void sortAircraft(List<Aircraft> aircrafts) {
-        Collections.sort(aircrafts);
-    }
-
-
-    private void sortUsingComparator(List<EnrichedFlight> flights) {
         flights.sort(Comparator.comparingInt(EnrichedFlight::getTimeInAir));
+        printFlightList(flights);
     }
+
+
 
     private void printAircraftList(List<Aircraft> aircrafts) {
         for (Aircraft aircraft : aircrafts) {
-            System.out.println(aircraft.getIcao24() + " " + aircraft.getModel());
+            System.out.println(aircraft);
         }
     }
 
