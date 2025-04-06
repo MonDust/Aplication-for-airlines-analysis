a72e687fc9d991c0e2e8a06b9e1f9891900b93da
StaPink <stapin.4@gmail.com>
2025-04-06 18:19:51 +0200

    Fix filter to grouping tool

--- Airline_Analyzer/src/main/java/pg/edu/pl/lsea/TestMain.java
+++ Airline_Analyzer/src/main/java/pg/edu/pl/lsea/TestMain.java
@@ -38,27 +38,24 @@ public class TestMain {
 
 
         File fileAircrafts1 = new File("resources/aircraft-database-complete-2022-09.csv");
-        File fileFlights1 = new File("resources/flight_sample_2022-09-26.csv");
-        File fileFlights2 = new File("resources/flight_sample_2022-09-02.csv");
-        File fileFlights3 = new File("resources/flight_sample_2022-09-03.csv");
-        File fileFlights4 = new File("resources/flight_sample_2022-09-04.csv");
+//        File fileFlights1 = new File("resources/flight_sample_2022-09-26.csv");
+//        File fileFlights2 = new File("resources/flight_sample_2022-09-02.csv");
+//        File fileFlights3 = new File("resources/flight_sample_2022-09-03.csv");
+//        File fileFlights4 = new File("resources/flight_sample_2022-09-04.csv");
         File fileFlights5 = new File("resources/flight_sample_2022-09-01.csv");
 
 
         long start = System.currentTimeMillis();
-        dataLoader.loadFlightsToStorage(fileFlights1);
-        dataLoader.loadFlightsToStorage(fileFlights2);
-        dataLoader.loadFlightsToStorage(fileFlights3);
-        dataLoader.loadFlightsToStorage(fileFlights4);
+//        dataLoader.loadFlightsToStorage(fileFlights1);
+//        dataLoader.loadFlightsToStorage(fileFlights2);
+//        dataLoader.loadFlightsToStorage(fileFlights3);
+//        dataLoader.loadFlightsToStorage(fileFlights4);
         dataLoader.loadFlightsToStorage(fileFlights5);
-        dataLoader.loadFlightsToStorage(fileFlights1);
-        dataLoader.loadFlightsToStorage(fileFlights2);
+//        dataLoader.loadFlightsToStorage(fileFlights1);
+//        dataLoader.loadFlightsToStorage(fileFlights2);
         long end = System.currentTimeMillis();
         System.out.println(end-start);
 
-        // Clean the data
-        nullRemover.TransformAircrafts(aircrafts);
-        nullRemover.TransformFlights(flights);
 
         start = System.currentTimeMillis();
         dataLoader.loadAircraftsToStorage(fileAircrafts1);
@@ -69,11 +66,8 @@ public class TestMain {
         System.out.println(DataStorage.getInstance().countAircrafts());
         System.out.println(DataStorage.getInstance().countFlights());
         // Feature engineering (adding new features/properties to Flights)
-        List<EnrichedFlight> enrichedFlights;
-        enrichedFlights = dataEnrichment.CreateEnrichedListOfFlights(flights);
 
-        // Sort the data
-        correlationCalculator.analyzeDataForDashbord(aircrafts, enrichedFlights);
+        List<Aircraft> aircrafts = DataStorage.getInstance().getAircrafts();
 
         List<Flight> flights = DataStorage.getInstance().getFlights();
         System.out.println(flights.stream().findAny());
@@ -84,6 +78,14 @@ public class TestMain {
         // --------------------------------------- SINGLE-THREADED ANALYSIS ---------------------------------------
         long single_analysis_start = System.currentTimeMillis();
 
+        // Clean the data
+        nullRemover.TransformAircrafts(aircrafts);
+        nullRemover.TransformFlights(flights);
+        List<EnrichedFlight> enrichedFlights;
+        enrichedFlights = dataEnrichment.CreateEnrichedListOfFlights(flights);
+
+        // Sort the data
+        correlationCalculator.analyzeDataForDashbord(aircrafts, enrichedFlights);
         // Firstly, group aircraft by models and store a list of flights for each model
         GroupingTool groupingTool = new GroupingTool();
         List<List<EnrichedFlight>> listOfLists = groupingTool.sortFlightsByModel(enrichedFlights, aircrafts);
