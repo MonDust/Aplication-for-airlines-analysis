9bffaa426bc55f3613edb7a3199b758fb0b859a3
O. Paszkiewicz <s187583@student.pg.edu.pl>
2025-04-22 13:59:51 +0000

    Adding information about the data needed to perform certain analyses.

--- frontend/src/main/java/pg/edu/pl/lsea/gui/choicewindows/ChoosingAnalysisWindow.java
+++ frontend/src/main/java/pg/edu/pl/lsea/gui/choicewindows/ChoosingAnalysisWindow.java
@@ -43,16 +43,27 @@ public class ChoosingAnalysisWindow extends BaseChoosingWindow{
     /**
      * Perform analysis on available data based on the selected analysis type.
      <ul>
-     *   <li> 1 - Sequential vs Parallel performance</li>
-     *   <li> 2 - Sort by number of flights</li>
-     *   <li> 3 - Sort by total time of flights</li>
-     *   <li> 4 - Most popular operators</li>
-     *   <li> 5 - Most popular models</li>
-     *   <li> 6 - Plot average time</li>
-     *   <li> 7 - Percentage of long flights</li>
+     *   <li> 1 - All analysis </li>
+     *   <li> 2 - Most popular operators</li>
+     *   <li> 3 - Most popular models</li>
+     *   <li> 4 - Plot average time</li>
+     *   <li> 5 - Percentage of long flights</li>
      * </ul>
      */
     private void analyse() {
+        // TODO - get bellow information: API
+        // 2 - flights grouped by the operator
+        // List<Output> : ICAO (representing specific operator) + value: number of flights.
+        // -> later: take specific aircraft information by ICAO (operator)
+        // 3 - flights grouped by the model
+        // List<Output> : ICAO (representing specific model) + value: number of flights.
+        // -> later: take specific aircraft information by ICAO (model)
+        // 4 - getting average time for top operators, then plotting it
+        // List<Output> : ICAO (representing specific operator) + value: avarage time of flight per operator.
+        // -> later: take specific aircraft information by ICAO (operator)
+        // 5 - getting percentage of long flights for top operators
+        // List<Output> : ICAO (representing specific operator) + value: percentage of long flights.
+        // -> later: take specific aircraft information by ICAO (operator)
         String selectedName = (String) analysisTypeComboBox.getSelectedItem();
         int selectedType = AnalysisTypeConstants.NAME_TO_ID.getOrDefault(selectedName, 0);
         mainPanel.showAnalysis(selectedType);
